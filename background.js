bbcdcec25980653bd51089e292bd29f174067523
anoushka-shri <anoushk2@andrew.cmu.edu>
2022-09-10 20:42:46 -0400

    extra changes

--- background.js
+++ background.js
@@ -1,8 +1,17 @@
 // background.js
+console.log("background running!");
 
-let color = '#3aa757';
+// let color = '#3aa757';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
+
+function getInfo(whenFinished)
+{
+  chrome.runtime.onMessage.addEventListener(function(message){
+    console.log("message received!");
+    whenFinished(message);
+  });
+}
--- popup.js
+++ popup.js
@@ -1,24 +1,69 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+// https://markb.uk/building-a-simple-google-chrome-extension.html for window.addEventListener
+// window.addEventListener("load", async function() {
+//   let [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+//   this.document.getElementById("url").innerHTML = tab.url;
+  // pageDocs = chrome.scripting.executeScript({target: {tabId: tab.id}, func: getInfoFromBody});
+  // this.document.getElementById("title").innerHTML = 
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+  // function getInfoFromBody()
+  // {
+  //   return document.body.innerHTML;
+  // }
+// });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: setPageBackgroundColor,
-    });
+//if we don't need url
+// window.addEventListener("load", function() {
+//   this.document.getElementById("title").innerHTML = window.document.body.innerHTML;
+// });
+
+// chrome.runtime.onMessage.addListener(processUserSearches);
+
+// function processUserSearches(searchData, sender, response){
+//   document.getElementById("url").innerHTML = "lameeee!"
+//   document.getElementById("title").innerHTML = "hello!" //searchData.results
+//   console.log("hello!")
+//   response({'hi': "hello!!!!!"})
+// }
+
+//https://markb.uk/building-a-simple-google-chrome-extension.html
+window.addEventListener("load", setupFunction);
+
+function setupFunction(eventTarget) {
+  this.document.getElementById("title").innerHTML = "window setup!";
+  chrome.runtime.getBackgroundPage(function(currPage) {
+    this.document.getElementById("cookies").innerHTML = "cookkiiiiieee";
+    currPage.getInfo(processInfo);
   });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
+
+  function processInfo(query) {
+    this.document.getElementById("url").innerHTML = "urlurlrul";
+    response(query.query);
   }
+}
+
+
+// Initialize button with user's preferred color
+// let changeColor = document.getElementById("changeColor");
+
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+//   document.getElementById("color").innerHTML=color;
+// });
+
+// // When the button is clicked, inject setPageBackgroundColor into current page
+// changeColor.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       func: setPageBackgroundColor,
+//     });
+//   });
+  
+//   // The body of this function will be executed as a content script inside the
+//   // current page
+//   function setPageBackgroundColor() {
+//     chrome.storage.sync.get("color", ({ color }) => {
+//       document.body.style.backgroundColor = color;
+//     });
+//   }
